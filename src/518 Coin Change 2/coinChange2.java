b7da24303d55607de9e447e7e92303212ac0327d
nishadabhay18 <abhayn927@gmail.com>
2026-05-01 21:55:09 +0530

    Update coinChange2.java

--- src/518 Coin Change 2/coinChange2.java
+++ src/518 Coin Change 2/coinChange2.java
@@ -8,25 +8,25 @@ class coinChange2 {
         if (idx == coins.length) return 0;
         int skip = countWays(coins, amount, idx + 1);
         int take = 0;
-        if (amount-coins[idx]>=0) take = countWays(coins, amount - coins[idx], idx);
+        if (amount - coins[idx] >= 0) take = countWays(coins, amount - coins[idx], idx);
         return skip + take;
     }
 
     // RECURSION+MEMOIZATION
     public int change(int amount, int[] coins) {
-        int n=coins.length;
-        int [][]dp=new int [n][amount+1];
-        for(int i=0;i<n;i++)
-            for(int j=0;j<=amount;j++) dp[i][j]=-1;
-        return countWays(coins, amount, 0,dp);
+        int n = coins.length;
+        int [][]dp = new int [n][amount+1];
+        for(int i = 0; i<n; i++)
+            for(int j = 0; j<=amount; j++) dp[i][j] = -1;
+        return countWays(coins, amount, 0, dp);
     }
-    private int countWays(int[] coins, int amount, int idx,int[][]dp) {
+    private int countWays(int[] coins, int amount, int idx, int[][]dp) {
         if (amount == 0) return 1;
         if (idx == coins.length) return 0;
         if(dp[idx][amount]!=-1) return dp[idx][amount];
         int skip = countWays(coins, amount, idx + 1,dp);
         int take = 0;
-        if (amount-coins[idx]>=0) take = countWays(coins, amount - coins[idx], idx,dp);
-        return dp[idx][amount]= skip + take;
+        if (amount - coins[idx] >= 0) take = countWays(coins, amount - coins[idx], idx, dp);
+        return dp[idx][amount] = skip + take;
     }
 }
