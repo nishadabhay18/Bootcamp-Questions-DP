4ea26442eaf1d5b0fd7839d18bb4a6c67bc93ab8
nishadabhay18 <abhayn927@gmail.com>
2026-05-06 18:49:08 +0530

    Update fibonacciNumber.java

--- src/509_Fibonacci_Number/fibonacciNumber.java
+++ src/509_Fibonacci_Number/fibonacciNumber.java
@@ -1,22 +1,25 @@
 class fibonacciNumber {
-    public static int fibRec(int n) {
+    // RECURSION TC-> O(2^n) SC-> O(n)
+    public int fib(int n) {
         if(n <= 1) return n;
-        return fibRec(n-1) + fibRec(n-2);
+        return fib(n-1) + fib(n-2);
     }
-
+ 
+    // RECURSION + MEMOIZATION TC-> O(n) SC-> O(n)+O(n)
     static int []dp;
-    public static int fibMemo(int n){
+    public int fib(int n){
         int []dp = new int[n+1];
         return fibo(n, dp);
     }
-    public static int fibo(int n, int []dp){
+    public int fibo(int n, int []dp){
         if(n <= 1) return n;
         if(dp[n] != 0) return dp[n];
         int ans = fibo(n-1, dp) + fibo(n-2, dp);
         return dp[n] = ans;
     }
 
-    public static int fibTab(int n){
+    // TABULATION TC-> O(n) SC-> O(1)
+    public int fib(int n){
         if(n == 0) return 0;
         if(n == 1) return 1;
         int []dp = new int[3];
